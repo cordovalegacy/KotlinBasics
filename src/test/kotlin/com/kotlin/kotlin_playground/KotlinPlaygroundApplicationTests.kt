180ed10c961a1c4f9be8afa37bc59b44f43998e9
Brendan Cordova <cordovalegacy19@gmail.com>
2024-01-19 06:15:14 -0600

    practice with kotlin val, var, fun, and classes

---
+++ src/test/kotlin/com/kotlin/kotlin_playground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.kotlin_playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
